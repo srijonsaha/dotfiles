c0a3ac063406a15ece9d39a9226609e589d2d318
Srijon Saha <srijon365@gmail.com>
2021-05-23 23:10:03 -0700

    Update sublime preferences symlink path

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sf "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/zsh/.zshrc" ~
 ln -sf "$DOTFILES_DIR/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
-ln -sf "$DOTFILES_DIR/sublime/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -sf "$DOTFILES_DIR/sublime/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
